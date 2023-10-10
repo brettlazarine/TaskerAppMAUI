dc6a10c8a8d79c6c902e5c5e88ee106c22361d98
Brett Lazarine <jblazarine@gmail.com>
2023-10-09 22:34:34 -0500

    initial commit

---
+++ TaskerApp/TaskerApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskerApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TaskerApp/TaskerApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TaskerApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
